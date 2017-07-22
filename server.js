256e1fd6c28e2ef0aed1a4c867db47d1d6be81d3
maximus136 <>
2017-07-23 03:06:08 +0530

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+var cors = require('cors');
+
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 8000,
   mongoose = require('mongoose'),
   ChatHistory = require('./api/models/ChatHistoryModel'),
   bodyParser = require('body-parser');
+  
+app.use(cors());
   
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://heroku_7rcrjj4s:fq0tkipu75hgi8h7jnpgn22f1b@ds133311.mlab.com:33311/heroku_7rcrjj4s'); 
